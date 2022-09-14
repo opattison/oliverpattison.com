f7a17e387173ebc71005b81673e46aecafd5106c
Oliver Pattison <oliverpattison@gmail.com>
2022-09-14 20:10:45 +0000

    major changes to how images are loaded - preparing for photo stories - subfolders for organizing photo projects - more complicated method for building urls for images

--- .eleventy.js
+++ .eleventy.js
@@ -35,19 +35,22 @@ module.exports = function(eleventyConfig) {
     secureURLToken: token
   });
   const basePath = "/images/";
-  
-  eleventyConfig.addShortcode("image_portfolio", (path, alt) => { 
+    
+  eleventyConfig.addShortcode("image_portfolio", (folder, path, alt) => { 
     const defaultWidth = 501;
     const defaultHeight = 334;
+    const fullPath = folder + "/" + path;
     
-    const src = client.buildURL(path, 
+    const srcLong = basePath + fullPath;
+    
+    const src = client.buildURL(fullPath, 
       {
         w: defaultWidth,
         h: defaultHeight
       }
     );
     
-    const srcset = client.buildSrcSet(path,
+    const srcset = client.buildSrcSet(fullPath,
       {},
       { widths: [300, 501, 699, 900, 1101, 1299, 1500] }
     );
@@ -55,16 +58,18 @@ module.exports = function(eleventyConfig) {
     const sizes = "(min-width: 35em) calc(50vw - 3em), (min-width: 28em) calc(50vw - 2em), calc(100vw - 4em)";
     
     if (process.env.NODE_ENV === 'development') {
-      return `<img src="${basePath}${path}" class="grid-image">`;
+      return `<img src="${srcLong}" class="grid-image">`;
     }
     else {
       return `<img src="${src}" srcset="${srcset}" sizes="${sizes}" alt="${alt}" width="${defaultWidth}" height="${defaultHeight}" class="grid-image">`;
     };
   });
   
-  eleventyConfig.addShortcode("image_profile", (path, alt) => {
+  eleventyConfig.addShortcode("image_profile", (folder, path, alt) => {
     const defaultWidth = 501;
     const defaultHeight = 334;
+    
+    const srcLong = basePath + path;
     
     const src = client.buildURL(path, 
       {
@@ -81,7 +86,7 @@ module.exports = function(eleventyConfig) {
     const sizes = "(min-width: 52em) 37.5vw, (min-width: 35em) calc(50vw - 3em), calc(100vw - 2em)";
     
     if (process.env.NODE_ENV === 'development') {
-      return `<img src="${basePath}${path}" class="profile">`;
+      return `<img src="${srcLong}" class="profile">`;
     }
     else {
       return `<img src="${src}" srcset="${srcset}" sizes="${sizes}" alt="${alt}" width="${defaultWidth}" height="${defaultHeight}" class="profile">`;
@@ -89,14 +94,14 @@ module.exports = function(eleventyConfig) {
   });
   
   eleventyConfig.addShortcode("image_og", (path) => {
-    const url = client.buildURL(path, {
+    const src = client.buildURL(path, {
       w: 1200,
       h: 630,
       fit: 'crop',
       crop: 'faces'
     });
     
-    return `${url}`;
+    return `${src}`;
   });
   
   return {
